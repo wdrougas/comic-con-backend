d8bc351cf7a56af05a0860389e4849c5defa5f27
github username <github email address>
2020-01-16 15:51:29 -0500

    first commit

---
+++ db/migrate/20200116204834_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :budget
+      t.string :image_url
+      t.string :box_office_revenue
+      t.string :description
+      t.string :name
+      t.string :rating
+      t.string :runtine
+
+      t.timestamps
+    end
+  end
+end
