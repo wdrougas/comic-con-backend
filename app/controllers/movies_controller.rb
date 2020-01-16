9fa2b9ed9682a0715a6539d22e4389f11bd8c107
github username <github email address>
2020-01-16 16:17:46 -0500

    changes made

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,19 @@
 class MoviesController < ApplicationController
+
+    def index
+        movies = Movie.all 
+        render json: movies.to_json(serialize_data)
+    end 
+
+    
+    private
+
+    def serialize_data
+        {:except => [:created_at,:updated_at]}
+    end 
+
+
+
 end
+
+
