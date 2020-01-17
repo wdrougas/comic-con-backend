942516667efb15e64c57f4f43f81d7422e1695db
Will Drougas <wdrougas@gmail.com>
2020-01-17 13:59:34 -0500

    users model seeded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all 
+        render json: users.to_json(serialize_data)
+    end 
+
+    
+    private
+
+    def serialize_data
+        {:except => [:created_at,:updated_at]}
+    end 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ require 'rest-client'
 
 
 Movie.destroy_all
+User.destroy_all
 
 movies = RestClient.get('https://comicvine.gamespot.com/api/movies/?api_key=1bc3aaafa2e58de60dce5a3f1b73c8f995e8b254&format=json&limit=200')
 
@@ -30,3 +31,18 @@ movie_array.each do |movie|
     )
 
 end 
+
+
+will = User.create(username: "wdrougas", profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Will.jpg')
+jose = User.create(username: "jromero" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Jose.jpg')
+trevor = User.create(username: "tjameson" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Trevor.jpg')
+chine = User.create(username: "canikwe" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Chine.jpg')
+sara = User.create(username: "ssmith" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Sara.jpg')
+kyle = User.create(username: "cakehole" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Kyle.jpg')
+jasur = User.create(username: "jabdullin" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Jasur.jpg')
+matt = User.create(username: "mbechtel" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/MattB.jpg')
+rob = User.create(username: "rheavner" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Matt.jpg')
+ryan = User.create(username: "rsmith" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Ryan.jpg')
+remi = User.create(username: "rremi" , profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Remi.jpg')
+sebastian = User.create(username: "ssebastian", profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Sebastian.jpg')
+young = User.create(username: "yhan", profile_photo: 'https://mandiokateam.com/sports-buddy/profile/Young.jpg')
