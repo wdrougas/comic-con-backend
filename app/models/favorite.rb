1d7fd3cf2dc7063dde9864f304ab3757c04c966f
github username <github email address>
2020-01-21 13:57:23 -0500

    changes made

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ApplicationRecord
     belongs_to :user
     belongs_to :movie 
+    validates :user_id, :movie_id , presence: true
+    validates :user_id, :movie_id , uniqueness: true 
 end
