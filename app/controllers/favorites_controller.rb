2103f8672fbc600f9e8fbd2891bf9f757f0c1f14
github username <github email address>
2020-01-21 09:39:33 -0500

    changes made

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,25 @@
 class FavoritesController < ApplicationController
+
+    def index
+        favorites = Favorite.all 
+        render json:favorites.to_json(serialized_data)
+    end 
+
+  
+    def create 
+        favorite = Favorite.create(strong_params)
+        render json:favorite.to_json(serialized_data)
+    end 
+  
+    private 
+
+    def strong_params 
+        params.require(:favorite).permit(:user_id , :movie_id)
+    end 
+
+    def serialized_data
+        {:except => [:created_at,:updated_at]}
+    end 
+
 end
+
