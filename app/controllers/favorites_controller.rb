92711b80e2c2d2f931d8139de32d206732eb1e35
Will Drougas <wdrougas@gmail.com>
2020-01-22 10:44:09 -0500

    favorite objects

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,11 +10,23 @@ class FavoritesController < ApplicationController
         favorite = Favorite.create(strong_params)
         if favorite.valid? 
         #  render json: favorite.to_json(serialized_data)
-        render json: {message: "Movie added to favorites!", movie: favorite.to_json(serialized_data)}
+        render json: {message: "Movie added to favorites!", favorite: favorite.to_json(serialized_data)}
          else 
         render json: { message: "Movie already added to favorites"}
          end
     end 
+
+    def show
+        favorite = Favorite.find(params[:id])
+        render json: favorite.to_json(serialized_data)
+    end
+
+    def destroy
+        favorite = Favorite.find(params[:id])
+        favorite.delete
+        byebug
+        render json: {message: "Movie removed from favorites"}
+    end
   
     private 
 
